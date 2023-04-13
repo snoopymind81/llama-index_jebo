28aa122a52a569ac056306f36580a897741697f7
Hyunsoo Kim <59876318+snoopymind81@users.noreply.github.com>
2023-04-13 15:14:42 +0900

    Update index.py

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader, LLMPredicto
 from langchain import OpenAI
 
 # This example uses text-davinci-003 by default; feel free to change if desired
-llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.7, model_name="text-davinci-003"))
+llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.7, model_name="text-davinci-002"))
 
 # Configure prompt parameters and initialise helper
 max_input_size = 4000
