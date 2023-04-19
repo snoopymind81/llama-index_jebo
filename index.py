5907db4b11d581abfaf25bb7dd10bee312de3971
Hyunsoo Kim <59876318+snoopymind81@users.noreply.github.com>
2023-04-19 13:34:28 +0900

    test_11

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 import os, streamlit as st
-
+import pandas as pd
 # Uncomment to specify your OpenAI API key here (local testing only, not in production!), or add corresponding environment variable (recommended)
 # os.environ['OPENAI_API_KEY']= ""
 
@@ -7,6 +7,8 @@ from pathlib import Path
 from llama_index import download_loader
 from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader, LLMPredictor, PromptHelper
 from langchain import OpenAI
+from langchain.document_loaders import DataFrameLoader
+
 
 # This example uses text-davinci-003 by default; feel free to change if desired
 llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-davinci-003"))
@@ -28,10 +30,16 @@ prompt_helper = PromptHelper(max_input_size, num_output, max_chunk_overlap)
 #documents = loader.load_data(file=Path('./data/jebo.xlsx'), column_name="nickname", column_name="contents", column_name="date", column_name="latitude", column_name="longitude", pandas_config={"sheet_name":"Sheet1"})
 #documents = loader.load_data(file=Path('./data/jebo.xlsx'), column_name=("nickname", "contents", "date", "latitude", "longitude"), pandas_config={"sheet_name":"Sheet1"})
 
-PandasCSVReader = download_loader("PandasCSVReader")
+# PandasCSVReader = download_loader("PandasCSVReader")
 
-loader = PandasCSVReader()
-documents = loader.load_data(file=Path('./data/jebo.csv'))
+
+df=pd.read_csv('./data/jebo.csv')
+
+
+loader = DataFrameLoader(df, page_content_column="nickname")
+documents = loader.load()
+#loader = PandasCSVReader()
+#documents = loader.load_data(file=Path('./data/jebo.csv'))
 
 
 
